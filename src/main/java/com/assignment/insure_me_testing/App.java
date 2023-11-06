ec575db715869cf2df325c7e9d1ddd07ab109fa8
Mainak Banerjee <mainak@Mainaks-MacBook-Pro.local>
2023-11-06 00:33:04 -0500

    Pushing the code to local repository

---
+++ src/main/java/com/assignment/insure_me_testing/App.java
@@ -0,0 +1,53 @@
+package com.assignment.insure_me_testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        //System.out.println( "Hello World!" );
+        
+        System.out.println("Hello, test case starting.");
+        //System.setProperty("webdriver.chrome.driver", "/Users/mainak/Documents/Devops-Bootcamp/Softwares/chromedriver-mac-x64/chromedriver");
+    	WebDriverManager.chromedriver().setup();
+    	
+        ChromeOptions chromeOptions = new ChromeOptions();
+        
+        chromeOptions.addArguments("--headless");
+        
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        driver.get("http://3.88.150.152:8081/contact.html");
+        System.out.println("Hello, test case executing.");
+        
+        driver.findElement(By.id("inputName")).sendKeys("Mainak");
+        driver.findElement(By.id("inputNumber")).sendKeys("9999999999");
+        driver.findElement(By.id("inputMail")).sendKeys("mainak@xyz.com");
+        driver.findElement(By.id("inputMessage")).sendKeys("Hello");
+        Thread.sleep(200);
+        
+        driver.findElement(By.id("my-button")).click();
+        Thread.sleep(200);
+        
+        if(driver.getPageSource().contains("Message Sent"))
+        {
+        	System.out.println("Hello, test case successful.");
+        }
+        else 
+        {
+        	System.out.println("Hello, test case failed.");
+        }
+        System.out.println("Hello, test case completed.");
+        driver.quit();
+    }
+}
